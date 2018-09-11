9b35813bc5621fdf6cb92001994c938872ab501c
Peter Horton <phorton@softstackfactory.org>
2018-09-11 04:14:30 +0000

    new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeekComponent } from './week/week.component';
+import { DayComponent } from './day/day.component';
+import { ThreemonthsComponent } from './threemonths/threemonths.component';
+import { SixmonthsComponent } from './sixmonths/sixmonths.component';
+import { YearComponent } from './year/year.component';
+import { FiveyearsComponent } from './fiveyears/fiveyears.component';
+import { MaxComponent } from './max/max.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { routes } from './app.router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeekComponent,
+    DayComponent,
+    ThreemonthsComponent,
+    SixmonthsComponent,
+    YearComponent,
+    FiveyearsComponent,
+    MaxComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
