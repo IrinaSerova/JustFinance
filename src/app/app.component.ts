4596b740a55a131bc1d913d271af14f81aff6a6c
Peter Horton <phorton@softstackfactory.org>
2018-09-18 06:24:24 +0000

    imported MessagesComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,35 @@ import { ApiService } from './api.service';
 export class AppComponent {
   title = 'stockDataSearch';
   dates = [];
+  targetSymbol: string;
+  timeSeries: any;
   // this.stock.getData(this.equity)
   constructor(private api: ApiService){
     api.getData().subscribe((data) =>{
       console.log(data);
       console.log(data['Time Series (Daily)']);
-      // this.targetSymbol = data[ 'Meta Data']['2.Symbol'];
+      this.targetSymbol = data['Meta Data']['2. Symbol'];
       
+      
+      // let data_open = data['list'].map(res => res.main.temp_max);
+
+
+
+      
+     
+      console.log('results', this.targetSymbol);
       // return (data: any) => data
       // .map((data: any) => get(data['Time Series (Daily)']))
       // console.log(data)
-      
-      // for (let result in data['Time Series (Daily)']){
-      //   console.log(`Returns ${this.result}`);
+      this.timeSeries = data['Time Series (Daily)'];
+      // for (let result in timeSeries){
+      // console.log(`Returns ${this.result}`);
         
         // this.res = data['Meta Data']['2.Symbol'];
         // console.log(`Returns ${this.res}`);
         // console.log(`Returns ${res}`);
       // }
+     // Create labels array
      
       
     }, err => {
